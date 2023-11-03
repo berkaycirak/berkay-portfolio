4b0aa44efbb564e6c822cede0f36c9c7591e273d
Berkay <berkaycirak_11@hotmail.com>
2023-11-03 16:40:04 +0300

    fix

--- src/models/SpaceHelmet.tsx
+++ src/models/SpaceHelmet.tsx
@@ -7,7 +7,6 @@ Title: Space Helmet
 */
 
 import * as THREE from "three";
-import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 import { GLTF } from "three-stdlib";
 
