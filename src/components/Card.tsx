04d686d6c5426cd59b513447d84e920f8e509bb4
Berkay <berkaycirak_11@hotmail.com>
2023-11-03 16:35:36 +0300

    feat:3d card animation is added

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,64 @@
-const Card = () => {
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface CardProps {
+  title: string;
+  description?: string;
+  testLink?: string;
+  techStack?: string[];
+}
+
+const Card = ({ title, description, testLink, techStack }: CardProps) => {
+  const [isFlipped, setIsFlipped] = useState<boolean>(false);
   return (
-    <div className="w-[250px] h-[300px] relative border rounded-lg flex items-center justify-center shadow-glass backdrop-blur-[20px] cursor-pointer group">
-      Pharmapp
+    <div className="[perspective:850px]">
+      <div
+        onClick={() => setIsFlipped(!isFlipped)}
+        className={twMerge(
+          "w-[300px] h-[400px] relative  rounded-lg flex items-center justify-center shadow-glass transition-[transform] duration-500 [transform-style:preserve-3d]  cursor-pointer group [&_div]:w-full [&_div]:h-full [&_div]:absolute [&_div]:[backface-visibility:hidden] [&_div]:rounded-lg select-none  ",
+          isFlipped && "[transform:rotateY(-180deg)]"
+        )}
+      >
+        {/* Frontside */}
+        <div
+          className={twMerge(
+            "bg-inherit bg-cover bg-center  bg-no-repeat text-center group  relative group overflow-hidden",
+            title.toLowerCase().includes("pharmto")
+              ? " bg-pharmto"
+              : title.toLowerCase().includes("heroes") && "bg-heroes"
+          )}
+        >
+          <div className="bg-black/70 h-full transition"></div>
+          <div className="flex items-center justify-center uppercase font-bold ">
+            {title}
+          </div>
+          <div className="absolute   transition  translate-y-[100%] group-hover:translate-y-[90%] font-bold">
+            <p className="text-orange-400">Click to see more!</p>
+          </div>
+        </div>
+        {/* Backside */}
+        <div className="bg-gray-700 [transform:rotateY(180deg)] p-2 flex flex-col justify-between items-center">
+          <p className="">{description}</p>
+
+          <span className="text-center">
+            <h5 className="mt-4 mb-2">Tech Stack</h5>
+            {techStack!.map((tech) => (
+              <img
+                src={`../src/assets/techIcons/${tech}.svg`}
+                className="w-[50px] h-[40px] mx-2 inline-block"
+              />
+            ))}
+          </span>
+
+          <a
+            href={testLink}
+            target="_blank"
+            className=" inline-block p-2 rounded-xl bg-green-400 text-black transition text-center  font-bold hover:scale-[1.05] w-full mb-8 "
+          >
+            Test App
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -13,23 +13,29 @@ const techStackArray: TechStackArray[] = [
 
 const TechStack = () => {
   return (
-    <section className="flex items-center justify-evenly" id="Tech Stack">
-      {techStackArray.map((tech, i) => (
-        <div
-          className="w-[100px] h-[100px] relative  backdrop-blur-[20px] cursor-pointer group"
-          key={i}
-        >
-          <img
-            src={`../src/assets/techIcons/${tech.name}.svg`}
-            alt={tech.name}
-            className="w-full h-full"
-          />
+    <section
+      className="flex flex-col items-center justify-center"
+      id="Tech Stack"
+    >
+      <h1 className="text-green-400 mb-24">Tech Stack</h1>
+      <div className="flex r w-full items-center justify-center gap-8 ">
+        {techStackArray.map((tech, i) => (
+          <div
+            className="w-[100px] h-[100px] relative  backdrop-blur-[20px] cursor-pointer group"
+            key={i}
+          >
+            <img
+              src={`../src/assets/techIcons/${tech.name}.svg`}
+              alt={tech.name}
+              className="w-full h-full"
+            />
 
-          <div className="absolute w-full text-center transition duration-500  translate-y-[-100%] opacity-0 group-hover:opacity-100 group-hover:translate-y-0 mt-4 text-green-400/80 font-bold">
-            <p>{tech.name!.charAt(0).toUpperCase() + tech.name!.slice(1)}</p>
+            <div className="absolute w-full text-center transition duration-500  translate-y-[-100%] opacity-0 group-hover:opacity-100 group-hover:translate-y-0 mt-4 text-green-400/80 font-bold">
+              <p>{tech.name!.charAt(0).toUpperCase() + tech.name!.slice(1)}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </section>
   );
 };
