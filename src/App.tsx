834b37dd5d13d26bebcc373fef59b8eabbe22345
berkaycirak <berkaycirak_11@hotmail.com>
2024-06-14 13:47:59 +0300

    feat:under construction indicator is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import TechStack from "./components/TechStack";
-import Projects from "./components/Projects";
-import SocialMediaContainer from "./components/SocialMediaContainer";
+import SocialMediaContainer from './components/SocialMediaContainer';
 
 function App() {
-  return (
-    <div className="">
-      <Navbar />
-      <div className="pt-[--navigation-height] px-6 lg:px-24 relative">
-        <Hero />
+	return (
+		<div className=''>
+			{/* <Navbar /> */}
+			<div className='pt-[--navigation-height] px-6 lg:px-24 relative'>
+				{/* <Hero />
         <Projects />
-        <TechStack />
-        <SocialMediaContainer />
-      </div>
-    </div>
-  );
+        <TechStack /> */}
+				<h1 className='animate-pulse text-center text-green-500'>
+					Under Construction...
+				</h1>
+				<SocialMediaContainer />
+			</div>
+		</div>
+	);
 }
 
 export default App;
